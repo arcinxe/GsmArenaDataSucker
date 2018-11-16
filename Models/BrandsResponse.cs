8a7e7214d03c67cded61e590bb56e9695bf4a161
Marcin Arkan <marcinxe@gmail.com>
2018-11-16 05:28:13 +0100

    Initial commit

---
+++ Models/BrandsResponse.cs
@@ -0,0 +1,27 @@
+using Newtonsoft.Json;
+
+namespace ConsoleApp.Models
+{
+    public class BrandsResponse
+    {
+        [JsonProperty("data")]
+        public Datum[] Data { get; set; }
+
+        [JsonProperty("error")]
+        public object Error { get; set; }
+        public class Datum
+        {
+            [JsonProperty("id")]
+            public long Id { get; set; }
+
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("slug")]
+            public string Slug { get; set; }
+
+            [JsonProperty("number_of_devices")]
+            public long NumberOfDevices { get; set; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json;
+using ConsoleApp.Models;
+using System.Linq;
+
+namespace Documents
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var brandsResponse = await client.GetAsync("http://localhost:8080/brands");
+            brandsResponse.EnsureSuccessStatusCode();
+            var brandsContent = brandsResponse.Content.ReadAsStringAsync();
+            // Console.WriteLine(content.Result);
+            var brands = JsonConvert.DeserializeObject<BrandsResponse>(brandsContent.Result);
+            // Console.WriteLine("Hello World!");
+            // System.Console.WriteLine(brands.Data.FirstOrDefault().Slug);
+            File.WriteAllText("Brands.txt", JsonConvert.SerializeObject(brands));
+            var phonesResponse = await client.GetAsync($"http://localhost:8080/devices/{brands.Data.FirstOrDefault().Slug}?page=1");
+            var phonesContent = phonesResponse.Content.ReadAsStringAsync();
+            var phones = JsonConvert.DeserializeObject<PhonesResponse>(phonesContent.Result);
+            File.WriteAllText("Phones.txt", JsonConvert.SerializeObject(phones));
+            // System.Console.WriteLine(brands.Data.Sum(b => b.NumberOfDevices));
+        }
+    }
+}
